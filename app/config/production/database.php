0e14d100c72dc34e3c197cfed5aab58ccfdbe8a8
Mike Vartanian <mike.vartanian@gmail.com>
2014-11-05 16:08:18 -0500

    changed back to localhost

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -22,7 +22,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'mikevartanian.me',
+			'host'      => 'localhost',
 			'database'  => 'financials',
 			'username'  => 'root',
 			'password'  => 'BzJF0kZXcO',
