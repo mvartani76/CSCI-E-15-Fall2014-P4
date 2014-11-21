40e92dbe009af8701aabdf183a5f01937dedcbdd
Mike Vartanian <mike.vartanian@gmail.com>
2014-11-20 19:49:55 -0500

    Corrected title section to be able to display different title for each page

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
+	@yield('header')
 		<meta charset="UTF-8">
-		<title>Financial Valuation Made Easy</title>
-			<link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
-			<link rel='stylesheet' href='/css/finwebapp.css' type='text/css'>
-	</head>
+		<title>@yield('title')</title>
+		<link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
+		<link rel='stylesheet' href='/css/finwebapp.css' type='text/css'>
+	
 	<body>
 		<div class="container">
 			<div class="page-header">
