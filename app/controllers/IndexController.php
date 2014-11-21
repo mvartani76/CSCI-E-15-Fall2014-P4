a2e571c55b248c58e415e44a69dfd68bb62b778f
Mike Vartanian <mike.vartanian@gmail.com>
2014-11-20 19:51:28 -0500

    Added postIndex() and getLogout()

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -3,4 +3,31 @@ class IndexController extends BaseController {
 	public function getIndex() {
 		return View::make('index');
 	}
+
+	/**
+	* Process the login form
+	* @return View
+	*/
+	public function postIndex() {
+		$credentials = Input::only('email', 'password');
+		if (Auth::attempt($credentials, $remember = true)) {
+			return Redirect::intended('/user-dashboard')->with('flash_message', 'Welcome Back!');
+		}
+		else {
+			return Redirect::to('/')
+				->with('flash_message', 'Log in failed; please try again.')
+				->withInput();
+		}
+		return Redirect::to('/user-dashboard');
+	}
+	/**
+	* Logout
+	* @return Redirect
+	*/
+	public function getLogout() {
+		# Log out
+		Auth::logout();
+		# Send them to the homepage
+		return Redirect::to('/');
+	}
 }
