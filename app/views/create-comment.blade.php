6b20440f6f1a9df462919f60991fd25dae67c45e
Mike Vartanian <mike.vartanian@gmail.com>
2014-12-06 00:34:57 -0500

    Initial add of create-comment view

---
+++ app/views/create-comment.blade.php
@@ -0,0 +1,37 @@
+<html>
+	<body>
+		@extends('master')
+
+		@section('content')
+
+		<div class="container">
+			<div class="col-sm-6 col-md-4 col-md-offset-4">
+				<h1 class="text-center">Create a New Comment</h1>
+
+				@if ( Session::has('flash_message') )			 
+				  <div class= "alert {{ Session::get('flash_type') }}">
+				      <h3>{{ Session::get('flash_message') }}</h3>
+				  </div>			  
+				@endif
+
+				@foreach($errors->all() as $message)
+					<div>{{ $message }}</div>
+				@endforeach
+				<div class="account-wall">
+					<div class="form-signin">
+						{{ Form::open(array('url' => '/create-comment/'.$user->id)) }}
+
+							{{ Form::select('Project', $project, Null, array('class' => 'form-control', 'required', 'autofocus'),'default') }}
+							{{ Form::select('User', array('' => 'Please Select Intended User') + User::lists('username'), Null, array('class' => 'form-control', 'placeholder' => 'User', 'required', 'autofocus')) }}
+
+							{{ Form::textarea('Comment', Null, array('class' => 'my-form-control', 'placeholder' => 'Comment', 'required', 'autofocus')) }}</br></br>
+							{{ Form::submit('Create Comment', array('class'=>'btn btn-lg btn-primary btn-block')) }}
+
+						{{ Form::close() }}
+					</div>
+				</div>
+			</div>
+		</div>
+		@stop
+	</body>
+</html>
