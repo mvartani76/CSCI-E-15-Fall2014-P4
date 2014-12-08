6b1c78d48c50a242470fd1b773102815970aaac8
Mike Vartanian <mike.vartanian@gmail.com>
2014-12-08 16:50:08 -0500

    Updated the seeder file to latest table structure

--- app/database/seeds/CommentTableSeeder.php
+++ app/database/seeds/CommentTableSeeder.php
@@ -1,29 +1,39 @@
 <?php
-class CommentsTableSeeder extends Seeder {
+class CommentTableSeeder extends Seeder {
 	public function run() {
 		# Clear the tables to a blank slate
 		DB::statement('SET FOREIGN_KEY_CHECKS=0'); # Disable FK constraints so that all rows can be deleted, even if there's an associated FK
 		DB::statement('TRUNCATE comments');
 
-		# Users
-	    $comment1 = new Comments();
-		$comment1->comment = 'Hey. Nice work on Project 1! What do you think about changing the end year to 2020?';
+		# Comments
+	    $comment1 = new Comment();
+	    $comment1->comment_title = 'Change End Year?';
+		$comment1->comment_text = 'Hey. Nice work on Project 1! What do you think about changing the end year to 2020?';
+		$comment1->intended_user = 5;
 		$comment1->save();
 
-		$comment1 = new Comments();
-		$comment1->comment = 'I am not sure I agree with the discount rate on Project 2';
-		$comment1->save();
+		$comment2 = new Comment();
+		$comment2->comment_title = 'Discount Rate Question';
+		$comment2->comment_text = 'I am not sure I agree with the discount rate on Project 2';
+		$comment2->intended_user = 4;
+		$comment2->save();
 
-		$comment1 = new Comments();
-		$comment1->comment = 'We need to add Revenues from Customer 1 to Project 3';
-		$comment1->save();
+		$comment3 = new Comment();
+		$comment3->comment_title = 'Customer 1 Revenues to Project 3';
+		$comment3->comment_text = 'We need to add Revenues from Customer 1 to Project 3';
+		$comment3->intended_user = 2;
+		$comment3->save();
 
-		$comment1 = new Comments();
-		$comment1->comment = 'Lets remove travel expenses from Project 4';
-		$comment1->save();
+		$comment4 = new Comment();
+		$comment4->comment_title = 'Project 4 Travel Expenses';
+		$comment4->comment_text = 'Lets remove travel expenses from Project 4';
+		$comment4->intended_user = 3;
+		$comment4->save();
 
-		$comment1 = new Comments();
-		$comment1->comment = 'Project 5 looks good. Lets submit to Frank.';
-		$comment1->save();
+		$comment5 = new Comment();
+		$comment5->comment_title = 'Looks Good!';
+		$comment5->comment_text = 'Project 5 looks good. Lets submit to Frank.';
+		$comment5->intended_user = 1;
+		$comment5->save();
 	}
 }
