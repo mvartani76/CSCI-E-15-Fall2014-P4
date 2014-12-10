05e3ac23ee60dd60c1006927bb8d5581fdb252e3
Mike Vartanian <mike.vartanian@gmail.com>
2014-12-10 12:24:32 -0500

    Removed sending_users() and intended_users() as we are just using users() for the pivot table. Added functionality to remove associated members in pivot tables when a comment is deleted.

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -19,19 +19,27 @@ class Comment extends Eloquent implements UserInterface, RemindableInterface {
     public $timestamps = true;
 
 
-    public function sending_users()
+    public function users()
     {
     	return $this->belongsToMany('User');
-    }
-
-    public function intended_users()
-    {
-        return $this->belongsToMany('User');
     }
 
 	public function projects() {
 
         return $this->belongsToMany('Project');
     }
+
+    # Model events...
+    # http://laravel.com/docs/eloquent#model-events
+    public static function boot() {
+        parent::boot();
+        static::deleting(function($comment) {
+            DB::statement('DELETE FROM comment_project WHERE comment_id = ?', array($comment->id));
+        });
+        static::deleting(function($comment) {
+            DB::statement('DELETE FROM comment_user WHERE comment_id = ?', array($comment->id));
+        });
+    }
+
 
 }
